ecd3272b843d84f0cdaeeef53bfa7d7a2beb5beb
Youngbeom Choi <helloybz@gmail.com>
2018-07-25 22:31:15 +0900

    Make html parsing method

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 PICKLE_PATH = os.path.join(BASE_DIR, 'pickles')
-DATA_PATH = os.path.join(BASE_DIR, 'data')
-
+DATA_PATH = os.path.join(BASE_DIR, 'data') if os.name == 'posix' else os.path.join(
+    os.path.dirname(os.path.dirname(os.path.dirname(BASE_DIR))), 'storage', 'data', 'wiki2vec')
