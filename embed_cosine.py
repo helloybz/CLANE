351db8085f63a40d4498a13af402b61997b1c04e
helloybz <helloybz@gmail.com>
2019-05-29 02:22:32 +0900

    Add embed with cosine

---
+++ embed_cosine.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+import pdb
+import pickle
+
+from numpy import inf
+from sklearn.metrics.pairwise import cosine_similarity
+import torch
+from torch.nn import functional as F
+
+from dataset import CoraDataset, CiteseerDataset
+from settings import PICKLE_PATH
+from embed import update_embedding
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str)
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--gamma', type=float, default=0.74)
+parser.add_argument('--tolerence_Z', type=int, default=30)
+parser.add_argument('--model_tag', type=str)
+config = parser.parse_args()
+
+
+
+if __name__ == "__main__":
+    device = torch.device(f'cuda:{config.gpu}') if config.gpu else torch.device('cpu')
+    gamma = config.gamma
+    tolerence_Z = config.tolerence_Z
+    if config.dataset == 'cora':
+        graph = CoraDataset(device=device)
+    elif config.dataset == 'citeseer':
+        graph = CiteseerDataset(device=device)
+
+    sim_metric = lambda x,y: F.cosine_similarity(x,y,dim=-1)
+
+    min_distance = inf
+    tolerence = tolerence_Z
+    while tolerence != 0:
+        distance = update_embedding(graph, sim_metric, gamma)
+
+        if min_distance > distance:
+            min_distance = distance
+            tolerence = tolerence_Z
+        else:
+            tolerence -= 1
+
+    pickle.dump(graph.Z.cpu().numpy(), open(os.path.join(PICKLE_PATH, 'embedding', config.model_tag),'wb'))
+
