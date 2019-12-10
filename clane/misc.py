78b79b9760c2cd88c41652e5a9efd153b12414e6
YoungbeomChoi <helloybz@gmail.com>
2019-12-10 18:32:22 +0900

    Gather lecacy

--- clane/misc.py
+++
@@ -1,8 +0,0 @@
-def get_model_tag(config):
-    return f'{config.dataset}' \
-           f'{"_aprx" if config.aprx else ""}' \
-           f'_g{config.gamma}_lr{config.lr}' \
-           f'_rf{config.reduce_factor}' \
-           f'_rt{config.reduce_tol}' \
-           f'_pt{config.tol_P}' \
-           f'_zt{config.tol_Z}'
