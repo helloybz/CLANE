1afc6437d4f5f2dc048ff853d2cbd3753f1b13c5
Youngbeom Choi <helloybz@gmail.com>
2021-11-02 15:35:22 +0900

    Implement the suggested embedding method Fixes #12

---
+++ tests/test_embedder.py
@@ -0,0 +1,26 @@
+import unittest
+from pathlib import Path
+from clane import similarity
+from clane.embedder import Embedder
+
+from clane.graph import Graph
+
+
+class TestEmbedder(unittest.TestCase):
+    def setUp(self):
+        self.data_root = Path('zachary').absolute()
+        self.d = 16
+
+        self.g = Graph(
+            data_root=self.data_root,
+            embedding_dim=self.d,
+        )
+        self.embedder = Embedder(
+            graph=self.g,
+            similarity_measure=similarity.CosineSimilarity(),
+            gamma=0.74,
+        )
+
+    def test_iterate_if_embeddings_are_updated(self):
+        self.embedder.iterate()
+        self.assertNotEqual((self.g.Z - self.g.C).sum(), 0)
