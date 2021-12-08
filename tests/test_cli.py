041fc0b27f3c178033f870ffd6c4031414bf42de
Youngbeom Choi <helloybz@gmail.com>
2021-12-08 23:59:36 +0900

    Fix codes

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -26,21 +26,20 @@ class TestCLI(unittest.TestCase):
                 "--save_history"
             ])
 
-    def test_cli_embedding(self):
+    def test_cli_embedding_cosine(self):
         embedding(self.args_cosine)
-        Z = torch.from_numpy(np.load(Path("./test_output/Z_0.npy")))
+        Z = torch.from_numpy(np.load(Path("./test_output/0/Z_0.npy")))
         self.assertEqual(Z.shape[0], 34)
         self.assertEqual(Z.shape[1], 2)
-        embedding(self.args_asymm)
-        Z = torch.from_numpy(np.load(Path("./test_output/Z_0.npy")))
+        Z = torch.from_numpy(np.load(Path("./test_output/Z.npy")))
         self.assertEqual(Z.shape[0], 34)
         self.assertEqual(Z.shape[1], 2)
 
-    def test_cli_with_save_all(self):
-        embedding(self.args_cosine)
-        self.assertGreater(len(list(Path('./test_output').glob('./*_*.npy'))), 1)
+    def test_cli_embedding_asymmetric(self):
         embedding(self.args_asymm)
-        self.assertGreater(len(list(Path('./test_output').glob('./*_*.npy'))), 1)
+        Z = torch.from_numpy(np.load(Path("./test_output/Z.npy")))
+        self.assertEqual(Z.shape[0], 34)
+        self.assertEqual(Z.shape[1], 2)
 
     def tearDown(self) -> None:
         import shutil
